ee4ad3d7ca7bd91988ed116b7e34e721e684e209
ajamavdv <140681264+ajamavdv@users.noreply.github.com>
2024-04-19 01:12:46 +0100

    Create زخرفة.js

---
+++ plugins/زخرفة.js
@@ -0,0 +1,40 @@
+//اوبيتو عمك ✨🐉
+function handler(m, { text }) {
+  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+  m.reply(teks.replace(/[a-z]/gi, v => {
+      return { 
+          'a': '𝐴',
+          'b': '𝐵',
+          'c': '𝐶',
+          'd': '𝐷',
+          'e': '𝐸',
+          'f': '𝐹',
+          'g': '𝐺',
+          'h': '𝐻',
+          'i': '𝐼',
+          'j': '𝐽',
+          'k': '𝐾',
+          'l': '𝐿',
+          'm': '𝑀',
+          'n': '𝑁',
+          'o': '𝛩',
+          'p': '𝑃',
+          'q': '𝑄',
+          'r': '𝑅',
+          's': '𝑺',
+          't': '𝑇',
+          'u': '𝑈',
+          'v': '𝑉',
+          'w': '𝑊',
+          'x': '𝑋',
+          'y': '𝑌',
+          'z': '𝑍', 
+      }[v.toLowerCase()] || v
+  }))
+}
+handler.help = ['ＯＢＩＴＯ']
+handler.tags = ['ＯＢＩＴＯ']
+handler.command =  /^(زخرفة)$/i
+
+export default handler
+> `ᏫᏰᎥᎿᏫ`
